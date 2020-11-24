d1e54a3fe07aa5f8dd275e70f18a3450d785c836
gray-noizers <hankun970104@gmail.com>
2020-11-24 19:19:15 +0900

    ユーザー管理機能実装

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -17,110 +17,110 @@ RSpec.describe User, type: :model do
       it "nicknameが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.nickname = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "family_nameが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.family_name = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "family_nameが全角仮名漢字以外だと登録できない" do
         user = FactoryBot.build(:user)
         user.family_name = "test"
-        user.valid?
+        expect(user).valid?
       end
 
       it "first_nameが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.first_name = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "first_nameが全角仮名漢字以外だと登録できない" do
         user = FactoryBot.build(:user)
         user.first_name = "test"
-        user.valid?
+        expect(user).valid?
       end
 
       it "family_name_furiganaが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.family_name_furigana = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "family_name_furiganaが半角英数字以外だと登録できない" do
         user = FactoryBot.build(:user)
         user.family_name_furigana = "あああああああ"
-        user.valid?
+        expect(user).valid?
       end
 
       it "first_name_furiganaが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.first_name_furigana = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "first_name_furiganaが半角英数字以外だと登録できない" do
         user = FactoryBot.build(:user)
         user.first_name_furigana = "あああああああ"
-        user.valid?
+        expect(user).valid?
       end
 
       it "passwordが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.password = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "passwordが5文字以下だと登録できない" do
         user = FactoryBot.build(:user)
         user.password = "12345a"
-        user.valid?
+        expect(user).valid?
       end
 
       it "passwordが半角英数字混同出ないと登録できない" do
         user = FactoryBot.build(:user)
         user.password = "あえいうえおあお"
-        user.valid?
+        expect(user).valid?
       end
 
       it "password_confirmationが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.password_confirmation = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "password_confirmationがpasswordと一致していないと登録できない" do
         user = FactoryBot.build(:user)
         user.password_confirmation != user.password
-        user.valid?
+        expect(user).valid?
       end
 
       it "birthdayが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.birthday = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "emailが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.email = ""
-        user.valid?
+        expect(user).valid?
       end
 
       it "メールアドレスは一意性である必要がある" do
         user = FactoryBot.build(:user)
         user.save
         another_user = FactoryBot.build(:user)
-        another_user.valid?
+        expect(another_user).valid?
       end
 
       it "emailには@が含まれている必要がある" do
         user = FactoryBot.build(:user)
         user.email != "[@]"
-        user.valid?
+        expect(user).valid?
       end
 
   end
