7d5c70cdca732a23ab17fc63d2613ed273ec11e6
gray-noizers <hankun970104@gmail.com>
2020-11-24 14:52:58 +0900

    ユーザー管理機能実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
     factory :user do
       nickname              {"test"}
-      family_name           {"test"}
-      first_name            {"test"}
-      family_name_furigana  {"test"}
-      first_name_furigana   {"test"}
+      family_name           {"中南"}
+      first_name            {"未来"}
+      family_name_furigana  {"ナカミナミ"}
+      first_name_furigana   {"ミライ"}
       email                 {"test@example"}
-      password              {"000000"}
+      password              {"0000aaaa"}
       password_confirmation {password}
       birthday              {"1997/01/04"}
     end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
+  context "ユーザー管理機能ができる時" do
+    it "全ての要素が記入されていれば登録できる" do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
+
+  end
+
+    
+
   context "ユーザー新規登録ができない時" do
       it "nicknameが空だと登録できない" do
         user = FactoryBot.build(:user)
@@ -16,9 +26,21 @@ RSpec.describe User, type: :model do
         user.valid?
       end
 
+      it "family_nameが全角仮名漢字以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.family_name = "test"
+        user.valid?
+      end
+
       it "first_nameが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.first_name = ""
+        user.valid?
+      end
+
+      it "first_nameが全角仮名漢字以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name = "test"
         user.valid?
       end
 
@@ -28,15 +50,39 @@ RSpec.describe User, type: :model do
         user.valid?
       end
 
+      it "family_name_furiganaが半角英数字以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.family_name_furigana = "あああああああ"
+        user.valid?
+      end
+
       it "first_name_furiganaが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.first_name_furigana = ""
         user.valid?
       end
 
+      it "first_name_furiganaが半角英数字以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name_furigana = "あああああああ"
+        user.valid?
+      end
+
       it "passwordが空だと登録できない" do
         user = FactoryBot.build(:user)
         user.password = ""
+        user.valid?
+      end
+
+      it "passwordが5文字以下だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.password = "12345a"
+        user.valid?
+      end
+
+      it "passwordが半角英数字混同出ないと登録できない" do
+        user = FactoryBot.build(:user)
+        user.password = "あえいうえおあお"
         user.valid?
       end
 
@@ -64,7 +110,20 @@ RSpec.describe User, type: :model do
         user.valid?
       end
 
-    end
+      it "メールアドレスは一意性である必要がある" do
+        user = FactoryBot.build(:user)
+        user.save
+        another_user = FactoryBot.build(:user)
+        another_user.valid?
+      end
+
+      it "emailには@が含まれている必要がある" do
+        user = FactoryBot.build(:user)
+        user.email != "[@]"
+        user.valid?
+      end
+
+  end
   
   
 
