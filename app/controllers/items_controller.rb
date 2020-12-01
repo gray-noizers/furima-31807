d5178e63f72daf10928f19ef8026f53478f1d821
gray-noizers <hankun970104@gmail.com>
2020-12-01 11:35:33 +0900

    destroyアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit]
-    before_action :item_find, only: [:show, :edit, :update]
+    before_action :item_find, only: [:show, :edit, :update, :destroy]
 
     def index
         @items = Item.all.order("created_at DESC")
@@ -33,6 +33,14 @@ class ItemsController < ApplicationController
             redirect_to root_path
         else
             render :edit
+        end
+    end
+
+    def destroy
+        if @items.destroy
+            redirect_to root_path
+        else
+            render :show
         end
     end
 
