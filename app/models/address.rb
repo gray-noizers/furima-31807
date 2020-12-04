7e3e505969bbef6ce3b9cbbe1e13d89f8512ec41
gray-noizers <hankun970104@gmail.com>
2020-12-04 11:12:24 +0900

    商品が購入されているかいないかでの条件分岐

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,11 +2,11 @@ class Address < ApplicationRecord
     belongs_to :order
 
     with_options presence: true do
-        validates :postal_code
+        validates :postal_code, format: { with: ^\d{3}-\d{4}$ } 
         validates :city
         validates :banchi
         #validates :building
-        validates :tel, length: { maximum: 11 }
+        validates :tel, length: { maximum: 11 },  format: { with: ^0\d{10}$ } 
     end
     validates :prefecture_id, numericality: { other_than: 0 }
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -6,15 +6,12 @@ class Log
         validates :postal_code
         validates :city
         validates :banchi
-        #validates :building
         validates :tel
     end
     validates :prefecture_id, numericality: { other_than: 0 }
 
     def save
-        # ユーザーの情報を保存し、「user」という変数に入れている
         order = Order.create(user_id: user_id, item_id: item_id)
-        # 住所の情報を保存
         Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, banchi: banchi, building: building, tel: tel, order_id: order.id)
     end
 
