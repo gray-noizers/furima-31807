ee094ca056ee903eba2b1cb4bfb8c133438ef95d
gray-noizers <hankun970104@gmail.com>
2020-12-07 16:03:21 +0900

    issue解消3

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_113448) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.integer "prefecture_id", null: false
+    t.integer "prefecture", null: false
     t.string "city", null: false
     t.string "banchi", null: false
     t.string "building"
